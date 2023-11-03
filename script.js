6405848479d338be4356fc981524bb028089c93c
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-04 01:35:21 +0200

    Worked on script a bit, got interupted, mouseover function is not working properly. Start there next time

--- script.js
+++ script.js
@@ -4,12 +4,28 @@ const hide = document.querySelector("#hide");
 const clear = document.querySelector("#clear");
 const display1 = document.querySelector("#display1");
 const display2 = document.querySelector("#display2");
+const color = document.querySelector("#color");
+
+
 display1.textContent = size.value;
 display2.textContent=size.value;
-let boxNumber =size.value;
+let boxNumber = size.value;
+
+color.oninput = function (){
+  board.style.backgroundColor = color.value;
+}
 
 createGrid(boxNumber);
 
+// Work on mouseover, ain't right yet, lines below
+
+const board = document.querySelector(".board");
+board.addEventListener("mouseover", changeColor);
+
+function changeColor(){
+  board.style.backgroundColor = `${color.value}`
+}
+// mouseover lines above don't work
 size.oninput = function (){
   display1.textContent=this.value;
   display2.textContent=this.value;
