85d70586737eaa5a3a3176ba9b565dcf5a4fedf6
Nikolaos Emmanouilidis <121699424+NikEmman@users.noreply.github.com>
2023-11-05 12:14:23 +0200

    Project complete.
    Fixed the mouseover and added the clear fn. added the styles/font

--- script.js
+++ script.js
@@ -5,41 +5,66 @@ const clear = document.querySelector("#clear");
 const display1 = document.querySelector("#display1");
 const display2 = document.querySelector("#display2");
 const color = document.querySelector("#color");
+const board = document.querySelector(".board");
+
 
 
 display1.textContent = size.value;
-display2.textContent=size.value;
+display2.textContent = size.value;
 let boxNumber = size.value;
-
-color.oninput = function (){
-  board.style.backgroundColor = color.value;
-}
 
 createGrid(boxNumber);
 
-// Work on mouseover, ain't right yet, lines below
-
-const board = document.querySelector(".board");
-board.addEventListener("mouseover", changeColor);
-
-function changeColor(){
-  board.style.backgroundColor = `${color.value}`
-}
-// mouseover lines above don't work
-size.oninput = function (){
-  display1.textContent=this.value;
-  display2.textContent=this.value;
-  boxNumber=this.value;
+size.oninput = function () {
+  display1.textContent = this.value;
+  display2.textContent = this.value;
+  boxNumber = this.value;
   createGrid(boxNumber);
 }
 
 function createGrid(num) {
-  rightContainer.innerHTML="";
-    for (let x = 0; x < num**2; x++) {
-      const board = document.createElement("div");
-      board.classList = "board";
-        rightContainer.appendChild(board);
-    }
-    rightContainer.style.width =`${num}em`;
+  rightContainer.innerHTML = "";
+  for (let x = 0; x < num ** 2; x++) {
+    const board = document.createElement("div");
+    board.classList = "board";
+    board.style.backgroundColor = "white";
+    board.addEventListener("mouseover", function () {
+      this.style.backgroundColor = `${color.value}`;
+    });
+    rightContainer.appendChild(board);
+  }
+  rightContainer.style.width = `${num}em`;
 }
+function clearAll() {
+  const allBoards = document.querySelectorAll(".board");
+
+  for (let i = 0; i < allBoards.length; i++) {
+    allBoards[i].style.backgroundColor = "white";
+  }
+}
+
+clear.addEventListener("click", clearAll);
+hide.addEventListener("click", hideBorder)
+
+function hideBorder() {
+  const allBoards = document.querySelectorAll(".board");
+  if (allBoards[0].style.border === "1px solid black") {
+    hide.innerHTML = "Show grid lines";
+    for (let i = 0; i < allBoards.length; i++) {
+      allBoards[i].style.border = "none"
+    }
+  }
+  else {
+    hide.innerHTML = "Hide grid lines"
+    for (let i = 0; i < allBoards.length; i++) {
+      allBoards[i].style.border = "1px solid black"
+    }
+  }
+}
+
+
+
+
+
+
 
